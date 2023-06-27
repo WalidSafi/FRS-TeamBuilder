ac6723cfbb95f3b3f7d3edae25a43335a9a13a38
WalidSafi <32139490+WalidSafi@users.noreply.github.com>
2023-06-26 20:45:23 -0400

    Railway Test Build
    Test to try to deploy to railway

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import React, { useState, useEffect} from 'react'
-import axios, { AxiosError } from 'axios'
+import axios from 'axios'
+import './index.css'
+import Navbar from './NavigationBar/Navbar.jsx'
+import { BrowserRouter as Router, Route, Routes, Redirect } from 'react-router-dom'
+import background from './Images/background.png'
+import TeamBuilder from './TeamBuilderComponents/TeamBuilder'
+import TeamBuilderId from './TeamBuilderComponents/TeamBuilderId'
+import TeamBuilderAuthor from './TeamBuilderComponents/TeamBuilderAuthor'
+import NotFound from './NotFoundComponent/NotFound'
 
 function App() {
 
   const [data, setData] = useState([{}])
 
   useEffect(() => {
-    axios('/members')
+    axios('/team')
       .then((response) => {
+        console.log(response.data)
         setData(response.data)
       }
       )
@@ -15,13 +24,29 @@ function App() {
       
   return (
     <div>
-      {(typeof data.members === 'undefined') ? (
+        <Navbar />
+        <Router>
+            <Routes>
+                <Route path="/teamBuilder" element={<TeamBuilder />} />
+                <Route path="/teamBuilder/:id" element={<TeamBuilderId />} />
+                <Route path="/teamBuilder/author" element={<TeamBuilderAuthor />} />
+                <Route path="/NotFound" element={<NotFound />} />
+             </Routes>
+        </Router>
+      {(typeof data === 'undefined') ? (
         <div>loading...</div>
       ) : (
-          data.members.map((member, index) => (
-            <p key={index}>{member}</p> 
-          ))
-      )}     
+        <div id='MainContainer' class='relative bg-white p-10'>
+          <div id='TeamBuilderContainerHome' class='w-full'>
+            <img src={background}/>
+            <div class='mx-auto max-w-screen-2xl px-2 sm:px-6 lg:px-10'>
+              <h1 class='hidden sm:ml-6 sm:block'>TEAM BUILDER</h1>
+              <p class='text-3xl font-bolt underline'>{data.id}</p>
+              <p class='text-3xl font-bolt underline'>{data.author}</p>
+            </div>
+          </div>
+        </div>
+      )}      
     </div>
   )
 }
---
+++ client/src/TeamBuilderComponents/TeamBuilderId.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function TeamBuilderId() {
+  return (
+    <h1>TeamBuilderId</h1>
+  )
+}
