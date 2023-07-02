b05040c5c1bb8271d260cabb476bd7aeee5fe94e
WalidSafi <32139490+WalidSafi@users.noreply.github.com>
2023-07-02 13:38:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ def hello():
     return 'Hello, World!'
 
 if __name__ == "__main__":
-    app.run(debug=True, port=int(os.environ.get("PORT", 5000)))
+    app.run(host='0.0.0.0',debug=True, port=int(os.environ.get("PORT", 5000)))
